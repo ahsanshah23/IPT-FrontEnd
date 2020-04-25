0a7616dfc864fba855767b449ff0989348223889
Ahsan <ahsanshah2398@gmail.com>
2020-04-25 09:51:24 -0700

    first commit

---
+++ IptProject/Controllers/FYP/FYP.cs
@@ -0,0 +1,19 @@
+﻿using IptProject.Models.Cafeteria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IptProject.Controllers
+{
+    public class FYP : Controller
+    {
+
+        public ActionResult WebPage1()
+        {
+            return View();
+        }
+    }
+}
